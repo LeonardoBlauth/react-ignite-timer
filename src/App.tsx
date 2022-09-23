f1bfa7986a202937c7ae09f1d1b829e823fbed9b
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2022-09-23 09:59:15 -0300

    Config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -6,6 +9,9 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
