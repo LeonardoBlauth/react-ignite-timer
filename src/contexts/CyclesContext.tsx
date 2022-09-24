7a8234ae075791f9de8b07670bd611c0b4830fe4
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2022-09-24 11:36:13 -0300

    Detach actions

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,12 @@
 import { createContext, ReactNode, useReducer, useState } from 'react'
-import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles'
+import {
+  ActionTypes,
+  addNewCycleAction,
+  interruptCycleAction,
+  markCurrentCycleAsFinishedAction,
+  resetActiveCycleIdToNullAction,
+} from '../reducers/cycles/actions'
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 
 interface CreateCycleData {
   task: string
@@ -43,17 +50,11 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: { activeCycleId },
-    })
+    dispatch(markCurrentCycleAsFinishedAction())
   }
 
   function resetActiveCycleIdToNull() {
-    dispatch({
-      type: ActionTypes.RESET_ACTIVE_CYCLE_TO_NULL,
-      payload: {},
-    })
+    dispatch(resetActiveCycleIdToNullAction())
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -66,19 +67,13 @@ export function CyclesContextProvider({
       startDate: new Date(),
     }
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: { newCycle },
-    })
+    dispatch(addNewCycleAction(newCycle))
 
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: { activeCycleId },
-    })
+    dispatch(interruptCycleAction())
   }
 
   return (
---
+++ src/reducers/cycles/actions.ts
@@ -0,0 +1,33 @@
+import { Cycle } from './reducer'
+
+export enum ActionTypes {
+  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
+  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+  RESET_ACTIVE_CYCLE_TO_NULL = 'RESET_ACTIVE_CYCLE_TO_NULL',
+}
+
+export function addNewCycleAction(newCycle: Cycle) {
+  return {
+    type: ActionTypes.ADD_NEW_CYCLE,
+    payload: { newCycle },
+  }
+}
+
+export function interruptCycleAction() {
+  return {
+    type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
+  }
+}
+
+export function markCurrentCycleAsFinishedAction() {
+  return {
+    type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
+  }
+}
+
+export function resetActiveCycleIdToNullAction() {
+  return {
+    type: ActionTypes.RESET_ACTIVE_CYCLE_TO_NULL,
+  }
+}
